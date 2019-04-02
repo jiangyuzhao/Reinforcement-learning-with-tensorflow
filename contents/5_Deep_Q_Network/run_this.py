83ce939d4052d400000b6c02073887753228e079
JiangYuzhao <jiangyuzhao@pku.edu.cn>
2019-04-02 22:12:16 +0800

    add some notes in Chinese.

--- contents/5_Deep_Q_Network/run_this.py
+++ contents/5_Deep_Q_Network/run_this.py
@@ -1,52 +1,55 @@
+"""
+Sarsa is a online updating method for Reinforcement learning.
+
+Unlike Q learning which is a offline updating method, Sarsa is updating while in the current trajectory.
+
+You will see the sarsa is more coward when punishment is close because it cares about all behaviours,
+while q learning is more brave because it only cares about maximum behaviour.
+"""
+
 from maze_env import Maze
-from RL_brain import DeepQNetwork
+from RL_brain import SarsaLambdaTable
 
 
-def run_maze():
-    step = 0
-    for episode in range(300):
+def update():
+    for episode in range(100):
         # initial observation
         observation = env.reset()
+
+        # RL choose action based on observation
+        action = RL.choose_action(str(observation))
+
+        # initial all zero eligibility trace
+        RL.eligibility_trace *= 0
 
         while True:
             # fresh env
             env.render()
 
-            # RL choose action based on observation
-            action = RL.choose_action(observation)
-
             # RL take action and get next observation and reward
             observation_, reward, done = env.step(action)
 
-            RL.store_transition(observation, action, reward, observation_)
+            # RL choose action based on next observation
+            action_ = RL.choose_action(str(observation_))
 
-            if (step > 200) and (step % 5 == 0):
-                RL.learn()
+            # RL learn from this transition (s, a, r, s, a) ==> Sarsa
+            RL.learn(str(observation), action, reward, str(observation_), action_)
 
-            # swap observation
+            # swap observation and action
             observation = observation_
+            action = action_
 
             # break while loop when end of this episode
             if done:
                 break
-            step += 1
 
     # end of game
     print('game over')
     env.destroy()
 
-
 if __name__ == "__main__":
-    # maze game
     env = Maze()
-    RL = DeepQNetwork(env.n_actions, env.n_features,
-                      learning_rate=0.01,
-                      reward_decay=0.9,
-                      e_greedy=0.9,
-                      replace_target_iter=200,
-                      memory_size=2000,
-                      # output_graph=True
-                      )
-    env.after(100, run_maze)
+    RL = SarsaLambdaTable(actions=list(range(env.n_actions)))
+
+    env.after(100, update)
     env.mainloop()
-    RL.plot_cost()
